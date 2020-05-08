966280d74d633728de79e28b494a1c9458824b55
josephabdelfattah <56602224+josephabdelfattah@users.noreply.github.com>
2020-05-07 19:58:38 -0700

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({body}, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.put("/api/transaction/:id", ({ params, body }, res) => {
+  console.log(body);
+  db.Transaction.findByIdAndUpdate(params.id)
+      .then(dbTransaction => {
+          res.json(dbTransaction);
+      })
+      .catch(err => {
+          res.json(err);
+      });
+});
+
+router.post("/api/transaction/bulk", ({body}, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({}).sort({date: -1})
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
+module.exports = router;
